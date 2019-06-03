10213c5e4fa3eae47dd380dd2dbf6aa789ea3aae
Jose Montero Jr <josemariodev@gmail.com>
2019-06-03 16:22:36 -0500

    data coming in

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.scss';
+import dummyData from './dummy-data';
+import UserList from './UserList/UserList';
 
 export class App extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      data: dummyData,
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1>test</h1>
+        <UserList userlist={this.state.data} />
       </div>
     );
   }
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PostContainer = props => {
-  return <div />;
+  return (
+    <div>
+      <h3>{props.postcontainer.username}</h3>
+      <h3>{props.postcontainer.likes}</h3>
+      <img src={props.postcontainer.thumbnailUrl} alt='image' />
+    </div>
+  );
 };
 
 PostContainer.propTypes = {};

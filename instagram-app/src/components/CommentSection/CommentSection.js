b70f20b72b415ceba231786385afc57fc942f1ef
Jose Montero Jr <josemariodev@gmail.com>
2019-06-03 14:16:07 -0500

    boilerplated

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CommentSection = props => {
+  return <div />;
+};
+
+CommentSection.propTypes = {};
+
+export default CommentSection;
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PostContainer = props => {
+  return <div />;
+};
+
+PostContainer.propTypes = {};
+
+export default PostContainer;
